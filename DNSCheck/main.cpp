12735a1dff5bd7fd25b1d4bf73737e4c60402733
Ubuntu64 test <15071267961@163.com>
2017-12-11 07:22:41 -0800

    something

--- DNSCheck/main.cpp
+++ DNSCheck/main.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <winsock2.h>
+#include <Iphlpapi.h>
+#include <windows.h>
+#include <string.h>
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "IPHLPAPI.LIB")
+#pragma pack(2)
 
 using namespace std;
 
